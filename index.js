d3bd6fe3dd40c39a002ad4f807883652f4e96c4a
Sydnee Sampson <sbsydnee161416@gmail.com>
2019-06-15 19:58:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,7 @@ function shout(string) {
 function whisper(string) { return string.toLowerCase()
 }
 
+function shoutHiToGrandma() console.log("Hi, Grandma!")
+function sayHelloToBrendan() {
+  console.log("Hello, Brendan!")
+}
